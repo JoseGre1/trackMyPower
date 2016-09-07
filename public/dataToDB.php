bedc736673999e82252e82196eaa50a9ca914ded
josegre1 <josegre1@hotmail.com>
2016-09-07 18:46:54 -0500

    add sniffing capacity and bootstrap files, attempt 2

--- public/dataToDB.php
+++ public/dataToDB.php
@@ -16,5 +16,5 @@ $result = mysqli_query($connect_todb,
 	"INSERT INTO `metcentraldata` (`id`, `temperature`, `wind_speed`, `voltage_med1`, `curr_med1`, `energy_med1`, `power_med1`, `latitude`, `longitude`) VALUES (NULL, '1', '1', '$voltage_med1', '$current_med1', '$energy_med1', '$power_med1', '$latitude', '$longitude');"
 );
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-echo $row
+echo $row;
 ?>
