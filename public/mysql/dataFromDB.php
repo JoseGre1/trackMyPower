93253e63db1f226971ccacc4d333996b8110c176
josegre1 <josegre1@hotmail.com>
2016-09-08 01:17:23 -0500

    check obtained data - attempt 8

--- public/mysql/dataFromDB.php
+++ public/mysql/dataFromDB.php
@@ -4,8 +4,9 @@ $n_records = 10;
 $result = mysqli_query($connect_todb, 
 	"SELECT `voltage_med1`, `curr_med1`, `power_med1`, `latitude`, `longitude` FROM `metcentraldata` ORDER BY id DESC LIMIT $n_records;"
 );
-$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-echo $row["voltage_med1"], ',', $row["curr_med1"],',',$row["power_med1"],',',$row["latitude"],',',$row["longitude"];
-$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-echo $row["voltage_med1"], ',', $row["curr_med1"],',',$row["power_med1"],',',$row["latitude"],',',$row["longitude"];
+for ($x = 1; $x<=n_records; $x++){
+	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	echo $row["voltage_med1"], ',', $row["curr_med1"],',',$row["power_med1"],',',$row["latitude"],',',$row["longitude"], ';';
+}
+
 ?>
