e883d86c74c91dd02a7e6cd1488b3c18b2d280d0
josegre1 <josegre1@hotmail.com>
2016-09-08 02:10:17 -0500

    check obtained data - attempt 12

--- public/mysql/dataFromDB.php
+++ public/mysql/dataFromDB.php
@@ -4,9 +4,18 @@ $n_records = 10;
 $result = mysqli_query($connect_todb, 
 	"SELECT `voltage_med1`, `curr_med1`, `power_med1`, `latitude`, `longitude` FROM `metcentraldata` ORDER BY id DESC LIMIT $n_records;"
 );
-for ($x = 1; $x<=$n_records; $x++){
-	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	echo $row["voltage_med1"], ',', $row["curr_med1"],',',$row["power_med1"],',',$row["latitude"],',',$row["longitude"], ';';
+// for ($x = 1; $x<=$n_records; $x++){
+// 	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+// 	echo $row["voltage_med1"], ',', $row["curr_med1"],',',$row["power_med1"],',',$row["latitude"],',',$row["longitude"], ';';
+// }
+
+
+ //create an array
+$emparray = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $emparray[] = $row;
 }
+echo json_encode($emparray);
 
 ?>
