b4892bd14cb3f7099225221ee86cf44a9ef3a948
josegre1 <josegre1@hotmail.com>
2016-09-08 05:05:01 -0500

    tables and JSON updates

--- public/js/create_table.js
+++ public/js/create_table.js
@@ -7,8 +7,28 @@ $(document).ready(function()
     });
     
     function processJSON(data){
-        JData = data;
-        createTable(JData);
+        loadScripts(["js/dbJsonData.js"],function(){alert('dbJsonData.js loaded successfuly')});
+        createTable(data);
+    }
+
+    function loadScripts(array,callback){
+    var loader = function(src,handler){
+        var script = document.createElement("script");
+        script.src = src;
+        script.onload = script.onreadystatechange = function(){
+        script.onreadystatechange = script.onload = null;
+            handler();
+        }
+        var head = document.getElementsByTagName("head")[0];
+        (head || document.body).appendChild( script );
+    };
+    (function(){
+        if(array.length!=0){
+            loader(array.shift(),arguments.callee);
+        }else{
+            callback && callback();
+        }
+    })();
     }
 
     function createTable(JData){
