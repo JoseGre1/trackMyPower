0eb2c5899b76f23797ed17e99fcd32844fc263e1
josegre1 <josegre1@hotmail.com>
2016-09-08 20:09:58 -0500

    tables and JSON updates

--- public/js/create_table.js
+++ public/js/create_table.js
@@ -7,35 +7,49 @@ $(document).ready(function()
             processJSON(data);
         });
     });
+
+    function reloadScript(data,id_script) {
+        // Check for existing script element and delete it if it exists
+        var js = document.getElementById(id_script);
+        if(js !== null) {
+            document.body.removeChild(js);
+        }
+        // Create new script element and load a script into it
+        js = document.createElement("script");
+        js.id = id_script;
+        var id_script2 = "script#".concat(id_script);
+        document.body.appendChild(js);
+        $(id_script2).append(data);
+    }
     
     function processJSON(data){
         createTable(data);
     }
 
     function createTable(data){
-            $("script#get_JSON_PHP").empty();  
-            $("script#get_JSON_PHP").append(data);
-            $("body").append($("#get_JSON_PHP").find("script:first"));
-            var strTable; 
-            strTable = '';
-            strTable += '<table border="2" class="table table-hover tbldata" style="padding:10px">';
+        reloadScript(data,"get_JSON_PHP");
+        //$("script#get_JSON_PHP").append(data);
+        //$("body").append($("#get_JSON_PHP").find("script:first"));
+        var strTable; 
+        strTable = '';
+        strTable += '<table border="2" class="table table-hover tbldata" style="padding:10px">';
+        strTable += '<tr>';
+        strTable += '<th >Voltage</th>';
+        strTable += '<th >Current</th>';
+        strTable += '<th >Power</th>';
+        strTable += '</tr>';    
+
+        $.each(JData, function(i, field)
+        {
             strTable += '<tr>';
-            strTable += '<th >Voltage</th>';
-            strTable += '<th >Current</th>';
-            strTable += '<th >Power</th>';
-            strTable += '</tr>';    
+            strTable += '<td >'+JData[i]['voltage_med1']+'</td>';
+            strTable += '<td >'+JData[i]['curr_med1']+'</td>';
+            strTable += '<td >'+JData[i]['power_med1']+'</td>';     
+            strTable += '</tr>';
+        });
+      
+        strTable += '</table>';        
 
-            $.each(JData, function(i, field)
-            {
-                strTable += '<tr>';
-                strTable += '<td >'+JData[i]['voltage_med1']+'</td>';
-                strTable += '<td >'+JData[i]['curr_med1']+'</td>';
-                strTable += '<td >'+JData[i]['power_med1']+'</td>';     
-                strTable += '</tr>';
-            });
-          
-            strTable += '</table>';        
-
-           $("div#charts").html(strTable);
+       $("div#charts").html(strTable);
     }
 });
