700011fb9d7cc4c0ce3fcecf8310edd3fa74dbf1
josegre1 <josegre1@hotmail.com>
2016-09-12 18:11:48 -0500

    dataToDB --> now we also read timestamp from Arduino --> Atemmpt 3

--- public/mysql/dataToDB.php
+++ public/mysql/dataToDB.php
@@ -11,7 +11,7 @@ $energy_med1 = htmlspecialchars($_GET["energy"]);
 $power_med1 = htmlspecialchars($_GET["power"]);
 $latitude = htmlspecialchars($_GET["latitude"]);
 $longitude = htmlspecialchars($_GET["longitude"]);
-$timestamp = htmlspecialchars($_GET["DateTime"]);
+$timestamp = htmlspecialchars($_GET["date_time"]);
 //mysql query --> insert data into db
 $result = mysqli_query($connect_todb, 
 	"INSERT INTO `metcentraldata` (`id`, `date_time`, `temperature`, `wind_speed`, `voltage_med1`, `curr_med1`, `energy_med1`, `power_med1`, `latitude`, `longitude`) VALUES (NULL,'$timestamp', '1', '1', '$voltage_med1', '$current_med1', '$energy_med1', '$power_med1', '$latitude', '$longitude');"
