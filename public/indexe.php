59c48b70755795f5fc7be1b3490c5229704dd2dd
Sebas <mpardo@ieee.org>
2016-09-12 10:06:29 -0500

    XLS Export App - Sebas - Attempt 1

---
+++ public/indexe.php
@@ -0,0 +1,14 @@
+ <html>  
+      <head>  
+           <title>esta shi a xls</title>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+      </head>  
+      <body>  
+             <h2 align="center">Ciaossu!</h2><br />    
+             <form method="post" action="xls.php" >  
+                 <input type="submit" name="export_excel" class="btn btn-success" value="Descargar excel" />  
+            </form>    
+      </body>  
+ </html>  
---
+++ public/xls.php
@@ -0,0 +1,48 @@
+ <?php
+
+$connect_todb = new mysqli('127.8.155.130','adminPzF8gsR','GsuEhey1gyxX','trackmypower');  
+ $output = '';  
+ if(isset($_POST["export_excel"]))  
+ {  
+      $sql = "SELECT * FROM mercentraldata ORDER BY id DESC";  
+      $result = mysqli_query($connect_todb, $sql);  
+      if(mysqli_num_rows($result) > 0)  
+      {  
+           $output .= '  
+                <table class="table" bordered="1">  
+                     <tr>  
+                          <th>id</th>
+                          <th>temperature</th>
+                          <th>wind_speed</th>
+                          <th>voltage_med1</th>
+                          <th>curr_med1</th>
+                          <th>energy_med1</th>
+                          <th>power_med1</th>
+                          <th>latitude</th>
+                          <th>longitude</th>  
+                     </tr>  
+           ';  
+           while($row = mysqli_fetch_array($result))  
+           {  
+                $output .= '  
+                     <tr>  
+                          
+                          <td>'.$row["id"].'</td>
+                          <td>'.$row["temperature"].'</td>
+                          <td>'.$row["wind_speed"].'</td>
+                          <td>'.$row["voltage_med1"].'</td>
+                          <td>'.$row["curr_med1"].'</td>
+                          <td>'.$row["energy_med1"].'</td>
+                          <td>'.$row["power_med1"].'</td>
+                          <td>'.$row["latitude"].'</td>
+                          <td>'.$row["longitude"].'</td>  
+                     </tr>  
+                ';  
+           }  
+           $output .= '</table>';  
+           header("Content-Type: application/xls");   
+           header("Content-Disposition: attachment; filename=download.xls");  
+           echo $output;  
+      }  
+ }  
+?>  
