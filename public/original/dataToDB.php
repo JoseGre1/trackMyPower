2642b2dee0a0edfbc8e0449401d8e51948950d77
josegre1 <josegre1@hotmail.com>
2016-09-14 16:35:22 -0500

    PHP changes according to new DB, new user: mpardo, new pssd: pardo1234

--- public/original/dataToDB.php
+++ public/original/dataToDB.php
@@ -3,7 +3,7 @@
 $user = htmlspecialchars($_GET["user"]);
 $pssd = htmlspecialchars($_GET["pssd"]);
 //mysql connection
-$connect_todb = new mysqli('127.8.155.130:3306',$user,$pssd,'trackmypower');
+$connect_todb = new mysqli('127.5.177.130:3306',$user,$pssd,'trackmypower');
 //obtain data from arduino
 $voltage_med1  = htmlspecialchars($_GET["voltage"]);
 $current_med1 = htmlspecialchars($_GET["current"]);
@@ -11,9 +11,14 @@ $energy_med1 = htmlspecialchars($_GET["energy"]);
 $power_med1 = htmlspecialchars($_GET["power"]);
 $latitude = htmlspecialchars($_GET["latitude"]);
 $longitude = htmlspecialchars($_GET["longitude"]);
+$timestamp = htmlspecialchars($_GET["date_time"]);
+$temperature = htmlspecialchars($_GET["temperature"]);
+$humidity = htmlspecialchars($_GET["humidity"]);
+//change datetime string --> date SQL format
+$date_time = date("Y-m-d H:i:s", strtotime($timestamp));
 //mysql query --> insert data into db
 $result = mysqli_query($connect_todb, 
-	"INSERT INTO `metcentraldata` (`id`, `temperature`, `wind_speed`, `voltage_med1`, `curr_med1`, `energy_med1`, `power_med1`, `latitude`, `longitude`) VALUES (NULL, '1', '1', '$voltage_med1', '$current_med1', '$energy_med1', '$power_med1', '$latitude', '$longitude');"
+	"INSERT INTO `metcentraldata` (`id`, `date_time`, `temperature`, `humidity` `wind_speed`, `voltage_med1`, `curr_med1`, `energasasay_med1`, `power_med1`, `latitude`, `longitude`) VALUES (NULL, '$date_time', '$temperature', '$humidity', '0' ,'$voltage_med1', '$current_med1', '$energy_med1', '$power_med1', '$latitude', '$longitude');"
 );
 $row = mysqli_fetch_array($result);
 echo $row;
