8873893006f0dbf394f11a799fac1478264aa2c5
Sebas <mpardo@ieee.org>
2016-09-22 00:00:32 -0500

    api2

--- public/index.php
+++ public/index.php
@@ -249,10 +249,10 @@
    
                 <div class="row">
 
-                <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="col-md-8 col-sm-12 col-xs-12">
                   <div class="x_panel">
                     <div class="x_title">
-                      <h2>Visitors location <small>geo-presentation</small></h2>
+                      <h2>Map<small>geo-presentation</small></h2>
                       <ul class="nav navbar-right panel_toolbox">
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                         </li>
@@ -273,7 +273,7 @@
                     <div class="x_content">
                       <div class="dashboard-widget-content">
                         
-                              <div  style="height: 500px;" id="map"></div>
+                              <div  style="height: 300px;" id="map"></div>
                                       <script type="text/javascript">
 
                                   var map;
