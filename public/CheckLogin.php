d8d4673d189cc929038f0f0d4d376f8313d17554
Sebas <mpardo@ieee.org>
2016-09-22 02:27:37 -0500

    login-home prueba 1

---
+++ public/CheckLogin.php
@@ -0,0 +1,22 @@
+
+<?php
+$connect = mysqli_connect("localhost", "mpardo", "pardo1234", "trackmypower");
+
+
+$usr = "admin";
+$psw = "password";
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$sql= "SELECT username, password FROM account where username='".$_POST["username"]."'";
+$result = mysqli_query($connect, $sql);
+$row = mysqli_fetch_array($result);  
+session_start();
+if (mysqli_num_rows($result) > 0 && $_POST['username']==$row['username'] && $_POST['password']==$row['password']) {
+  
+    header("Location: Home.html");}
+else { 
+
+header("Location: index.html");
+}
+ ?> 
---
+++ public/registDB.php
@@ -0,0 +1,10 @@
+<?php
+$connect_todb = new mysqli('localhost',"mpardo","pardo1234",'trackmypower');
+$username = $_POST["username"];
+$password = $_POST["password"];
+$result = mysqli_query($connect_todb, "INSERT INTO `account` (`username`, `password`) VALUES ('$username', '$password');"
+	);
+
+header("Location: login2.html");
+
+ ?>
