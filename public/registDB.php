d3c841093cc7861fb177e8d05fc0286535246ea8
Sebas <mpardo@ieee.org>
2016-09-22 04:57:08 -0500

    login-home prueba 4

--- public/registDB.php
+++ public/registDB.php
@@ -5,6 +5,6 @@ $password = $_POST["password"];
 $result = mysqli_query($connect_todb, "INSERT INTO `account` (`username`, `password`) VALUES ('$username', '$password');"
 	);
 
-header("Location: index.php");
+header("Location: index.html");
 
  ?>
