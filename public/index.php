4020fd8ce17d660b6c5d2f028935bd330d8bc8f8
Sebas <mpardo@ieee.org>
2016-09-21 23:51:19 -0500

    api

--- public/index.php
+++ public/index.php
@@ -247,7 +247,64 @@
 
 
    
+                <div class="row">
 
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                  <div class="x_panel">
+                    <div class="x_title">
+                      <h2>Visitors location <small>geo-presentation</small></h2>
+                      <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Settings 1</a>
+                            </li>
+                            <li><a href="#">Settings 2</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li><a class="close-link"><i class="fa fa-close"></i></a>
+                        </li>
+                      </ul>
+                      <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                      <div class="dashboard-widget-content">
+                        
+                              <div  style="height: 500px;" id="map"></div>
+                                      <script type="text/javascript">
+
+                                  var map;
+                                  function initMap() {
+
+                                    var myLatLng = {lat: 11.020121, lng: -74.851302};
+                                    map = new google.maps.Map(document.getElementById('map'), {
+                                      center: {lat: 11.020121, lng: -74.851302},
+                                      zoom: 18
+                                    });
+
+                                      var marker = new google.maps.Marker({
+                                      position: myLatLng,
+                                      map: map,
+                                      title: 'Hello World!'
+                                    });
+                                    marker.setMap(map);
+                                  }
+
+                                      </script>
+                                    <script async defer
+                                      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBemXPT9yR9ExhHqV3ZxpNdUbEjRuLctPo&callback=initMap">
+                                    </script>
+
+                       
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
                 
 
                                
