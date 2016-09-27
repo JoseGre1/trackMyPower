c58b5b759318a13800e79b8db820f4fd6a583168
josegre1 <josegre1@hotmail.com>
2016-09-27 12:59:39 -0500

    examples

--- public/Home.php
+++ public/Home.php
@@ -126,62 +126,63 @@
         <div class="top_nav">
           <div class="nav_menu">
             <nav class="navbar-fixed-top">
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
-
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/pardo.jpg" alt="">Mauricio
-                    <span class=" fa fa-angle-down"></span>
-                  </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="index.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul> 
-                </li>
-
-                <li role="presentation" class="dropdown">
-                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-envelope-o"></i>
-                    <span class="badge bg-green">1</span>
-                  </a>
-                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/diego.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>Diego Gómez</span>
-                          <span class="time">1 hour ago</span>
-                        </span>
-                        <span class="message">
-                          <br>
-                          Hola Mauro, <br>
-                          ¿Te satisface esta página? <br>
-                          S2 <br>
-                          DG<br>
-                          PD: Gracias por venir<br>
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="text-center">
-                        <a>
-                          <strong>See All Alerts</strong>
-                          <i class="fa fa-angle-right"></i>
+              <div class="container">
+                <div class="nav toggle">
+                  <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="">
+                    <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                      <img src="images/pardo.jpg" alt="">Mauricio
+                      <span class=" fa fa-angle-down"></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-usermenu pull-right">
+                      <li><a href="javascript:;"> Profile</a></li>
+                      <li>
+                        <a href="javascript:;">
+                          <span>Settings</span>
                         </a>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+                      </li>
+                      <li><a href="javascript:;">Help</a></li>
+                      <li><a href="index.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    </ul> 
+                  </li>
+
+                  <li role="presentation" class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                      <i class="fa fa-envelope-o"></i>
+                      <span class="badge bg-green">1</span>
+                    </a>
+                    <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                      <li>
+                        <a>
+                          <span class="image"><img src="images/diego.jpg" alt="Profile Image" /></span>
+                          <span>
+                            <span>Diego Gómez</span>
+                            <span class="time">1 hour ago</span>
+                          </span>
+                          <span class="message">
+                            <br>
+                            Hola Mauro, <br>
+                            ¿Te satisface esta página? <br>
+                            S2 <br>
+                            DG<br>
+                            PD: Gracias por venir<br>
+                          </span>
+                        </a>
+                      </li>
+                      <li>
+                        <div class="text-center">
+                          <a>
+                            <strong>See All Alerts</strong>
+                            <i class="fa fa-angle-right"></i>
+                          </a>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>  
             </nav>
           </div>
         </div>
