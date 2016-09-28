0d78dfb2b919851b0345df622c99d24244e525ae
josegre1 <josegre1@hotmail.com>
2016-09-28 15:25:49 -0500

    Fixing responsive[A

--- public/Home.php
+++ public/Home.php
@@ -204,12 +204,12 @@
         <div class="right_col" role="main">
           <!-- top tiles -->
           <div class="row tile_count">
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count pull-right">
               <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
               <div class="count">2,315</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count pull-right">
               <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
               <div class="count">7,325</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
